e196930156c76ce6b62d53963386da2c74d40305
Jacques Grelet <Jacques.grelet@ird.fr>
2018-01-10 16:11:46 +0100

    set all properties private use set|get<property> methods instead of set|get.<property> add constructor properties

--- +us191/@serial/serial.m
+++ +us191/@serial/serial.m
@@ -2,21 +2,22 @@ classdef serial < handle
   %Gps receive and print GPS data from serial RS232 line
   %   Detailed explanation goes here
   
-  properties (SetAccess = public, GetAccess = public)
-    Port
-    BaudRate = 4800
-    DataBits = 8
-    StopBits = 1
-    Parity = 'none'
-    Terminator = 'CR/LF'
-    Status = 'not connected'
-  end
-  
   properties (Access = private)
-    sp = [];
+    port
+    baudRate = 4800
+    dataBits = 8
+    stopBits = 1
+    parity = 'none'
+    terminator = 'CR/LF'
+    status = 'not connected'
+    echo = false
   end
   
   properties (Access = protected)
+    % sp is the matlab serial port, empty if not open
+    sp = [];
+    % sentence is the string return from serail port after reception of
+    % terminator, usually CR/LF
     sentence = []
   end
   
@@ -27,60 +28,102 @@ classdef serial < handle
   methods % public
     
     % class gps constructor
-    function obj = serial(port)
-      obj.Port = port;
-    end
+    function obj = serial(varargin)
+      if( isa(varargin{1}, 'char'))
+        % add test for a valid port with regexp,
+        % eg COMx under windows, /dev/ttyS* for Linux
+        obj.port = varargin{1};
+      else
+        error('us191:serial', '%s is an invalid serial port ', varargin{1});
+      end
+      if (nargin > 1 )
+        property_argin = varargin(2:end);
+        while length(property_argin) >= 2
+          property = property_argin{1};
+          value    = property_argin{2};
+          property_argin = property_argin(3:end);
+          switch lower(property)
+            case 'echo'
+              obj.setEcho(value);
+            case 'baudrate'
+              obj.setBaudRate(value);
+            case 'databits'
+              obj.setDataBits(value);
+            case 'stopbits'
+              obj.setStopBits(value);
+            case 'parity'
+              obj.setParity(value);
+            case 'terminator'
+              obj.setTerminator(value);
+            otherwise
+              error('us191:serial', 'Unknown property: %s', property);
+          end
+        end
+      end
+    end % end of serial constructor
     
     % open the serial port
     function open(obj)
       % if an instrument serial port stay in memory, clear it
-      thePort = instrfind('Port', obj.Port);
+      thePort = instrfind('Port', obj.port);
       if ~isempty(thePort)
         fclose(thePort);
         delete(thePort);
         clear thePort;
       end
       % create an serial port instance
-      obj.sp = serial(obj.Port, 'Baudrate', obj.BaudRate);
-      set(obj.sp, 'DataBits', obj.DataBits, 'StopBits', obj.StopBits,...
-        'Parity', obj.Parity);
+      obj.sp = serial(obj.port, 'Baudrate', obj.baudRate);
+      set(obj.sp, 'DataBits', obj.dataBits, 'StopBits', obj.stopBits,...
+        'Parity', obj.parity);
       fopen(obj.sp);
       obj.sp.BytesAvailableFcnMode = 'terminator';
-      obj.sp.Terminator = obj.Terminator;
+      obj.sp.Terminator = obj.terminator;
       % the callback
       obj.sp.BytesAvailableFcn = {@(src, event) obj.receive()};
     end
     
     % set the serial port name, eg COM9
-    function set.Port(obj, port)
-      obj.Port = port;
+    function setPort(obj, port)
+      obj.port = port;
+    end
+    
+    function port = getPort(obj)
+      port = obj.port;
     end
     
     % set the speed, eg 4800 or 9600 bds
-    function set.BaudRate(obj, baudRate)
+    function setBaudRate(obj, baudRate)
       switch baudRate
         case {300,600,1200,2400,4800,9600,19200,57600,112000}
-          obj.BaudRate = baudRate;
+          obj.baudRate = baudRate;
         otherwise
           error('MATLAB:gps:invalid baud rate value: %d', baudRate);
       end
     end
     
+    function baudRate = getBaudRate(obj)
+      baudRate = obj.baudRate;
+    end
+    
     % set the databits, eg 7 or 8
-    function set.DataBits(obj, dataBits)
+    function setDataBits(obj, dataBits)
       switch dataBits
         case {7,8}
-          obj.DataBits = dataBits;
+          obj.dataBits = dataBits;
         otherwise
           error('MATLAB:gps:invalid data bit value: %d', dataBits);
       end
     end
     
+    function dataBits = getDataBits(obj)
+      dataBits = obj.dataBits;
+    end
+    
     % set the parity, eg none, odd or even
-    function set.Parity(obj, parity)
+    function setParity(obj, parity)
       switch parity
         case {'none','odd','even'}
-          obj.Parity = parity;
+          obj.parity = parity;
         otherwise
           if isnumeric(parity)
             parity = num2str(parity);
@@ -89,34 +132,59 @@ classdef serial < handle
       end
     end
     
+    function parity = getParity(obj)
+      parity = obj.parity;
+    end
+    
     % set the stop bit, eg 1 or 2
-    function set.StopBits(obj, stopBits)
+    function setStopBits(obj, stopBits)
       switch stopBits
         case {1,2}
-          obj.StopBits = stopBits;
+          obj.stopBits = stopBits;
         otherwise
           error('MATLAB:gps:invalid stop bit value: %d', stopBits);
       end
     end
     
+    function stopBits = getStopBits(obj)
+      stopBits = obj.stopBits;
+    end
+    
     % set the terminator end line
-    function set.Terminator(obj, terminator)
+    function setTerminator(obj, terminator)
       switch terminator
         case {'CR/LF','CR','LF'}
-          obj.Terminator = terminator;
+          obj.terminator = terminator;
         otherwise
           error('MATLAB:gps:invalid terminator character, value : %s', terminator);
       end
     end
     
+    function terminator = getTerminator(obj)
+      terminator = obj.terminator;
+    end
+    
+    % set the serial port name, eg COM9
+    function setEcho(obj, echo)
+      if islogical(echo)
+        obj.echo = echo;
+      else
+        error('MATLAB:gps:invalid echo mode, value : %s', echo);
+      end
+    end
+    
+    function echo = getEcho(obj)
+      echo = obj.echo;
+    end
+    
     % return the status of the instrument
-    function status = get.Status(obj)
+    function status = getStatus(obj)
       if isa(obj.sp, 'instrument') && isvalid(obj.sp)
         status = get(obj.sp, 'Status');
       else
         status = 'not connected';
       end
-      obj.Status = status;
+      obj.status = status;
     end
     
     % close the serial port sp and delete from memory
@@ -130,16 +198,49 @@ classdef serial < handle
       obj.sp = [];
     end
     
+    % display serial object
+    % ---------------------
+    function disp(obj)
+      
+      % use local variables for displaying boolean
+      % ------------------------------------------
+      if obj.echo
+        theEcho = 'true';
+      else
+        theEcho = 'false';
+      end
+      
+      % display properties
+      % ------------------
+      fprintf('  Port:       ''%s''\n', obj.getPort);
+      fprintf('  BaudRate:   %d\n', obj.getBaudRate);
+      fprintf('  DataBits:   %d\n', obj.getDataBits);
+      fprintf('  StopBits:   %d\n', obj.getStopBits);
+      fprintf('  Parity:     ''%s''\n', obj.getParity);
+      fprintf('  Terminator: ''%s''\n', obj.getTerminator);
+      fprintf('  Status:     ''%s''\n', obj.getStatus);
+      fprintf('  Echo:       ''%s''\n', theEcho);
+      fprintf('\n');
+      
+      % diplay methods list in hypertext link
+      % -------------------------------------
+      disp('list of <a href="matlab:methods(''us191.serial'')">methods</a>');
+    end
+    
   end % end of public methods
   
   % receive read data from serial port and display it
   methods (Access = private)
+    
     function receive(obj, ~)
       obj.sentence = fgetl(obj.sp);
       notify(obj, 'sentenceAvailable');
-      %fprintf(1, 'Recu: ');
-      %fprintf(1, '%s\n',sentence);
+      if obj.echo
+        fprintf(1, 'Recu: ');
+        fprintf(1, '%s\n', obj.sentence);
+      end
     end
+    
   end % end of private methods
   
   methods (Static)
