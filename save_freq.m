a07b7ca31f22e2af930cc0a31e0fbe50ea19895d
Jacques Grelet <Jacques.Grelet@ird.fr>
2018-01-31 09:10:18 +0100

    add decomp voltage

--- save_freq.m
+++ save_freq.m
@@ -1,13 +1,11 @@
-%Function for calculating all frequency 
-
 function [ f ] = save_freq(bit,i)
-
-        data_f = bit(i:i+5);    %Get length of word for each frequency
-        
-        %Dimension of each byte
-        byte = [data_f(1),data_f(2);data_f(3),data_f(4);data_f(5),data_f(6)];
-        
-        byte_dec = hex2dec(byte);                           %Convert data hex to dec
-        f = byte_dec(1)*256+byte_dec(2)+byte_dec(3)/256;    %Calcul all frequency
-
-end
+            
+            data_f = bit(i:i+5);    %Get length of word for each frequency
+            
+            %Dimension of each byte
+            byte = [data_f(1),data_f(2);data_f(3),data_f(4);data_f(5),data_f(6)];
+            
+            byte_dec = hex2dec(byte);                           %Convert data hex to dec
+            f = byte_dec(1)*256+byte_dec(2)+byte_dec(3)/256;    %Calcul all frequency
+            
+        end
--- trame.m
+++ trame.m
@@ -1,6 +1,6 @@
 classdef trame < handle
     
-    %     properties
+    %     properties (Access = private)
     %
     %     end
     
@@ -10,20 +10,29 @@ classdef trame < handle
         function data_trame
             
             %Open files
-            fid1 = fopen('test-deckunit_2.txt','r');   
+            fid1 = fopen('test-deckunit_2.txt','r');
             fid2 = fopen('frequency.text','w');
+            fid3 = fopen('volt.text','w');
             
-            nb_sensor = 3;      %Selec number of sensor
-            dim = nb_sensor*6;  %Dimension trame with the nb of sensor      
+            nb_freq = 1;                %Selec number of frequency
+            nb_analog_volt = 8;
+            dim_volt = nb_analog_volt*3;
+            dim_freq = nb_freq*6;       %Dimension trame with the nb of frequency
             
             while ~feof(fid1)
-                trame = fgetl(fid1);      
+                trame = fgetl(fid1);
                 bit = lower(trame);
                 
-                for i = 1 : 6 : dim     
+                               
+                for i = 1 : 6 : dim_freq
                     [freq] = save_freq(bit,i);  %Get all frequency with funct
                     fprintf(fid2,'%f\n',freq);  %Save datas in file : fid2
                     %disp(freq);
+                end
+                
+                for j = dim_freq +1 : 3 : dim_freq + dim_volt
+                    [volt] = save_volt(bit,j);  %Get all voltage with funct
+                    fprintf(fid3,'%f\n',volt);  %Save datas in file : fid3
                 end
                 
             end
@@ -31,7 +40,9 @@ classdef trame < handle
             %Close files
             fclose(fid1);
             fclose(fid2);
+            fclose(fid3);
         end
     end
+    
 end
 
