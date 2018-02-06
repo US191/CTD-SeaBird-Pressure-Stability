c1d8190b78cc27d7db0a2d0492a322e03431032c
Jacques Grelet <Jacques.Grelet@ird.fr>
2018-02-06 16:01:28 +0100

    container.map and calcul pressure_temp and pressure

--- readXmlFile.m
+++ readXmlFile.m
@@ -1,35 +1,22 @@
-
-function readXmlFile
-
-properties
-filename
-%Class constructor
-    function obj = readXmlFile (varargin)
-        
-        obj.filename = varargin{1};
-        
-    end
-
+%Function for recover all coefficients of pressure sensor
+function coeff = readXmlFile(filename)
 
 %Read file xml
-file = xmlread(obj.filename);
+file = xmlread(filename);
 
-%Structure for recover coefficient of calibration
-name = {'CalibrationDate','SerialNumber','C1','C2','C3','D1',...
-    'D2','T1','T2','T3','T4','Slope','Offset','T5','AD590M','AD590B'};
+%Structure a map object coefficient of calibration
+keySet = {'PressureSensor','CalibrationDate','SerialNumber','C1','C2','C3','D1',...
+    'D2','T1','T2','T3','T4','Slope','Offset','T5','AD590M','AD590B','PressureSensor'};
 
-coefficients = {''};
-
-s = struct('name_element', name, 'coeff', coefficients);
-
-for i = 1:16
-    element = file.getElementsByTagName(s(i).name_element);
-    s(i).coeff = char(element.item(0).getFirstChild.getData);
-    disp(s(i).coeff);
+for i =1:18
+    element = file.getElementsByTagName(keySet(i));
+    value = element.item(0).getFirstChild.getData;
+    valueSet(i) = str2double(value);
 end
+% disp(keySet);
+coeff = containers.Map(keySet, valueSet);
 
 end
-
 
 
 
