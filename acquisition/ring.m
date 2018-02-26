a04bf6cfaff42e7318ad27855727097be51fa2a6
jacques Grelet <Jacques.Grelet@ird.fr>
2018-02-26 02:28:33 +0100

    add stats and static test

--- acquisition/ring.m
+++ acquisition/ring.m
@@ -4,14 +4,23 @@ classdef  ring < handle
   
   properties (SetAccess = private, GetAccess = public)
     data                    % buffer (initialized in constructor)
+  end
+  
+  properties (Access = private)
     bufferSize
     index = 1
+    list
+    average                     % mean or average
+    variance
+    med                       % median
+    stdev  % standard deviation
   end
   
   % constructor
   % -----------
   methods
     
+    % constructor with the buffer size
     function obj = ring(bufferSize)
       obj.bufferSize = bufferSize;
       obj.data = nan(bufferSize,1,'double')';
@@ -29,6 +38,30 @@ classdef  ring < handle
       obj.index = obj.index + 1;
     end
     
+    % compute save data, reset ring and compute result
+    function compute(obj)
+      obj.list = obj.data;
+      obj.clear;
+      obj.average = mean(obj.list);
+      obj.med = median(obj.list);
+      obj.variance = var(obj.list);
+      obj.stdev = std(obj.list);
+    end
+    
+    function result = getAverage(obj)
+      result = obj.average;
+    end
+    function result = getMedian(obj)
+      result = obj.med;
+    end
+    function result = getVariance(obj)
+      result = obj.variance;
+    end
+    function result = getStd(obj)
+      result = obj.stdev;
+    end
+    
+    
   end % end of public methods
   
   methods(Static)
@@ -42,8 +75,13 @@ classdef  ring < handle
         testRing.put(ind)
         disp(testRing.data);
       end
-      testRing.clear;
-      disp(testRing.data);
+      testRing.compute;
+      fprintf(1, 'Mean         = %f\n', testRing.getAverage);
+      fprintf(1, 'Median       = %f\n', testRing.getMedian);
+      fprintf(1, 'Variance     = %f\n', testRing.getVariance);
+      fprintf(1, 'Standard dev = %f\n', testRing.getStd);
+      % disp(testRing.data);
+      success = true;
     end
     
   end % end of static methods
