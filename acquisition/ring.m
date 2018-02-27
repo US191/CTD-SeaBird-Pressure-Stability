eb39e3f57fcf8aff46c3dbb0f92bbbb414a69cd4
jacques Grelet <Jacques.Grelet@ird.fr>
2018-02-27 09:32:27 -0100

    update ring with nanflag

--- acquisition/ring.m
+++ acquisition/ring.m
@@ -2,18 +2,28 @@ classdef  ring < handle
   %RING implement circular buffer for double
   %   Detailed explanation goes here
   
+  properties (Access = public)
+    nanflag = 'omitnan'  % NaN condition, specified as one of these values:
+    % 'includenan' — the variance of input containing NaN values is also NaN.
+    % 'omitnan' — all NaN values appearing in either the input array or
+    % weight vector are ignored.
+  end
+  
   properties (SetAccess = private, GetAccess = public)
     data                    % buffer (initialized in constructor)
+    ringSize
+    
   end
   
   properties (Access = private)
-    bufferSize
     index = 1
     list
-    average                     % mean or average
+    average              % mean or average
     variance
-    med                       % median
-    stdev  % standard deviation
+    med                  % median
+    stdev                % standard deviation
+    
+    
   end
   
   % constructor
@@ -21,20 +31,20 @@ classdef  ring < handle
   methods
     
     % constructor with the buffer size
-    function obj = ring(bufferSize)
-      obj.bufferSize = bufferSize;
-      obj.data = nan(bufferSize,1,'double')';
+    function obj = ring(ringSize)
+      obj.ringSize = ringSize;
+      obj.data = nan(ringSize,1,'double')';
     end
     
     function clear(obj)
-      obj.data = nan(obj.bufferSize,1,'double')';
+      obj.data = nan(obj.ringSize,1,'double')';
       obj.index = 1;
     end
     
     %  simply copy always all
     function  put(obj,value)
       assert(isa(value,'double'))
-      obj.data(mod(obj.index-1, obj.bufferSize)+1) = value;
+      obj.data(mod(obj.index-1, obj.ringSize)+1) = value;
       obj.index = obj.index + 1;
     end
     
@@ -42,10 +52,10 @@ classdef  ring < handle
     function compute(obj)
       obj.list = obj.data;
       obj.clear;
-      obj.average = mean(obj.list);
-      obj.med = median(obj.list);
-      obj.variance = var(obj.list);
-      obj.stdev = std(obj.list);
+      obj.average = mean(obj.list, obj.nanflag);
+      obj.med = median(obj.list, obj.nanflag);
+      obj.variance = var(obj.list, obj.nanflag);
+      obj.stdev = std(obj.list, obj.nanflag);
     end
     
     function result = getAverage(obj)
@@ -61,16 +71,21 @@ classdef  ring < handle
       result = obj.stdev;
     end
     
+    function set.nanflag(obj, nanflag)
+      if strcmp(nanflag, 'includenan') || strcmp(nanflag, 'ominan')
+        obj.nanflag = nanflag;
+      end
+    end
     
   end % end of public methods
   
   methods(Static)
     
-    % test
-    %-------
-    function success = test(bufferSize, maxValue)
-      success = false;
-      testRing = ring(bufferSize);
+    % testing
+    %--------
+    function success = test(ringSize, maxValue)
+      % success = false;
+      testRing = ring(ringSize);
       for ind = 1:maxValue
         testRing.put(ind)
         disp(testRing.data);
