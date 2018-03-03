d349d9dfdddbb94a7b5f7002f25b7b57d07597bc
Rousselot <pier.rousselot@gmail.com>
2018-03-03 17:59:18 -0100

    add pressure_offset

---
+++ pressure_offset.m
@@ -0,0 +1,32 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This script calculate offset for SeaBird pressure sensor from           %
+% ship barometer                                                          %
+% Autor: Pierre Rousselot - US191 IMAGO                                   %
+% Date: 03/03/2018                                                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear
+
+%% Variables
+% Environment
+P_baro   = 1008.6; % atmospheric pressure in mbar
+T_air    = 28.1;   % air temperature in °C
+P_CTD    = 0;      % pressure sensor in dbar
+
+% Ship
+H_baro   = 7.8;    % altitude baro in meter
+H_CTD    = 3.5;    % altitude CTD in meter
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Constants
+g        = 9.806;         % standard gravity
+R        = 287.08;        % dry air constant
+K        = 273.15;        % °C to Kelvin conversion
+Ap       = 14.7;          % approximate atmospheric pressure at sea level in psi 
+C        = 0.689476;      % factor conversion in dbar/psi
+
+%% Calcul
+P_atm    = (P_baro + ((P_baro * H_baro) / (29.2 * (T_air + K)))) * 0.01; % atmospheric pressure at sea level in dbar
+P_CTDa   = P_CTD + (Ap * C);                                   % absolute pressure from pressure seabird sensor
+
+Offset   = P_atm - P_CTDa;                                     % SeaBird pressure sensor offset
+disp(['Offset = ' num2str(round(Offset*100)/100)]);
